11ac82434f2c6e2c19f06e391e9fc14eabc20292
Udoh <udohabasionosunday@gmail.com>
2023-04-22 09:39:01 +0100

    Added Redux folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Provider } from "react-redux"; // NOTE: So, we imported this
+import { store } from "@/myReduxFiles/store";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
