0a69e8717024fe085ebee17670601d7e0cdca2f5
Udoh <udohabasionosunday@gmail.com>
2023-04-23 16:14:30 +0100

    Added code for the HomeScreen Carousel

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -9,48 +9,263 @@ export const HomeMainCarousel = () => {
         infiniteLoop={true}
         showThumbs={false}
         useKeyboardArrows={true}
-        animationHandler="fade"
+        animationHandler="slide"
         swipeScrollTolerance={5}
-        transitionTime={1000}
         showStatus={false}
         interval={5000}
         stopOnHover={false}
       >
-        <picture>
-          <source srcSet="/Home_page1-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page1-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page1-Large.webp" alt="A happy family" />
-        </picture>
+        <div className="grid">
+          <picture className="row-start-1 col-start-1">
+            <source srcSet="/Home_page1-Large.webp" media="(min-width:520px)" />
+            <source
+              srcSet="/Home_page1-small.webp"
+              media="(max-width:519.999px)"
+            />
+            <img src="/Home_page1-Large.webp" alt="A happy family" />
+          </picture>
 
-        <picture>
-          <source srcSet="/Home_page2-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page2-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page2-Large.webp" alt="A happy family" />
-        </picture>
+          <div
+            className="flex justify-center items-center flex-col row-start-1 col-start-1 self-end mb-10"
+            id="Carousel"
+          >
+            <p className="text-4xl font-bold mb-5">This is UDOHS</p>
 
-        <picture>
-          <source srcSet="/Home_page3-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page3-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page3-Large.webp" alt="A happy family" />
-        </picture>
+            <div
+              id="CarouselButtons"
+              className="w-[70%] max-w-[287px] mb-5 flex justify-center items-center flex-col"
+            >
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">SHOP MEN</span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
 
-        <picture>
-          <source srcSet="/Home_page4-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page4-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page4-Large.webp" alt="A happy family" />
-        </picture>
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP WOMEN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP CHILDREN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid">
+          <picture className="row-start-1 col-start-1">
+            <source srcSet="/Home_page2-Large.webp" media="(min-width:520px)" />
+            <source
+              srcSet="/Home_page2-small.webp"
+              media="(max-width:519.999px)"
+            />
+            <img src="/Home_page2-Large.webp" alt="A happy family" />
+          </picture>
+
+          <div
+            className="flex justify-center items-center flex-col row-start-1 col-start-1 self-end mb-10"
+            id="Carousel"
+          >
+            <p className="text-4xl font-bold mb-5">This is UDOHS</p>
+
+            <div
+              id="CarouselButtons"
+              className="w-[70%] max-w-[287px] mb-5 flex justify-center items-center flex-col"
+            >
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">SHOP MEN</span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP WOMEN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP CHILDREN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid">
+          <picture className="row-start-1 col-start-1">
+            <source srcSet="/Home_page3-Large.webp" media="(min-width:520px)" />
+            <source
+              srcSet="/Home_page3-small.webp"
+              media="(max-width:519.999px)"
+            />
+            <img src="/Home_page3-Large.webp" alt="A happy family" />
+          </picture>
+
+          <div
+            className="flex justify-center items-center flex-col row-start-1 col-start-1 self-end mb-10"
+            id="Carousel"
+          >
+            <p className="text-4xl font-bold mb-5">This is UDOHS</p>
+
+            <div
+              id="CarouselButtons"
+              className="w-[70%] max-w-[287px] mb-5 flex justify-center items-center flex-col"
+            >
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">SHOP MEN</span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP WOMEN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP CHILDREN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid">
+          <picture className="row-start-1 col-start-1">
+            <source srcSet="/Home_page4-Large.webp" media="(min-width:520px)" />
+            <source
+              srcSet="/Home_page4-small.webp"
+              media="(max-width:519.999px)"
+            />
+            <img src="/Home_page4-Large.webp" alt="A happy family" />
+          </picture>
+
+          <div
+            className="flex justify-center items-center flex-col row-start-1 col-start-1 self-end mb-10"
+            id="Carousel"
+          >
+            <p className="text-4xl font-bold mb-5">This is UDOHS</p>
+
+            <div
+              id="CarouselButtons"
+              className="w-[70%] max-w-[287px] mb-5 flex justify-center items-center flex-col"
+            >
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">SHOP MEN</span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full mb-5">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP WOMEN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+
+              <a href="#_" className="relative block text-lg group w-full">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative flex justify-center">
+                    SHOP CHILDREN
+                  </span>
+                </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
+            </div>
+          </div>
+        </div>
       </Carousel>
     </div>
   );
--- src/pages/HomeBody.js
+++ src/pages/HomeBody.js
@@ -2,56 +2,58 @@ import { HomeMainCarousel } from "./Carousel";
 
 export const HomeBody = () => {
   return (
-    <div className="">
+    <>
       <HomeMainCarousel />
 
-      <p>
-        This is <span>UDOHS</span>
-      </p>
-      <a>SHOP MEN</a>
-      <a>SHOP WOMAN</a>
-      <a>SHOP CHILDREN</a>
+      {/* <div className="flex justify-center items-center flex-col ">
+        <p className="text-4xl font-bold bg-white dark:bg-black pb-5">
+          This is UDOHS
+        </p>
 
-      <figure>
-        <picture>
-          <source srcSet="/Home_page1-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page1-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page1-Large.webp" alt="A happy family" />
-        </picture>
+        <div className="w-[70%] mb-5">
+          <a href="#_" class="relative block text-lg group w-full">
+            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span class="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span class="relative flex justify-center">SHOP MEN</span>
+            </span>
+            <span
+              class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </a>
+        </div>
 
-        <picture>
-          <source srcSet="/Home_page2-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page2-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page2-Large.webp" alt="A happy family" />
-        </picture>
+        <div className="w-[70%] mb-5">
+          <a href="#_" class="relative block text-lg group w-full">
+            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span class="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span class="relative flex justify-center">SHOP WOMEN</span>
+            </span>
+            <span
+              class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </a>
+        </div>
 
-        <picture>
-          <source srcSet="/Home_page3-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page3-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page3-Large.webp" alt="A happy family" />
-        </picture>
-
-        <picture>
-          <source srcSet="/Home_page4-Large.webp" media="(min-width:520px)" />
-          <source
-            srcSet="/Home_page4-small.webp"
-            media="(max-width:519.999px)"
-          />
-          <img src="/Home_page4-Large.webp" alt="A happy family" />
-        </picture>
-      </figure>
-
+        <div className="w-[70%] mb-5">
+          <a href="#_" class="relative block text-lg group w-full">
+            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span class="absolute left-0 w-full h-52 pr-4 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span class="relative flex justify-center">SHOP CHILDREN</span>
+            </span>
+            <span
+              class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </a>
+        </div>
+      </div> */}
       <p>EASY WORLDWIDE DELIVERY</p>
       <p>UDOHS DESIGNS</p>
-    </div>
+    </>
   );
 };
