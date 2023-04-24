5df919fecad7b0447ca3075096d44776b8ba210b
Udoh <udohabasionosunday@gmail.com>
2023-04-24 23:47:16 +0100

    Grid

--- src/pages/MenPage.js
+++ src/pages/MenPage.js
@@ -4,12 +4,8 @@ export const MenPage = () => {
       <h1 className="uppercase text-center font-bold text-3xl max-[500px]:text-2xl">
         Product Display
       </h1>
-      <div className="flex flex-wrap justify-center my-5">
-        <a
-          id="box"
-          href="#_"
-          className="flex-[0_1_40%] m-4 max-[340px]:m-3 max-w-[250px] min-[1029px]:max-w-[300px] "
-        >
+      <div className="theGrid my-5 mx-2">
+        <a id="box" href="#_" className="">
           <figure className="shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
             <picture>
               <source srcSet="/men 1.webp" />
@@ -17,88 +13,84 @@ export const MenPage = () => {
             </picture>
 
             <figcaption>
-              <p className="h-[44px] mb-3 overflow-hidden text-ellipsis">
+              <p className="h-[44px] my-3 overflow-hidden text-ellipsis text-center">
                 The Children&apos;s Place Girls Basic Super skinny Jean- Blue
               </p>
-              <p className="font-bold text-[#af4261] dark:text-[#f3ec78]">
+              <p className="text-center pb-3 text-xl font-bold text-[#af4261] dark:text-[#f3ec78]">
                 $26.2
               </p>
             </figcaption>
           </figure>
         </a>
 
-        <a
-          href="#_"
-          className="flex-[0_1_40%] m-4 max-[340px]:m-3 max-w-[250px] min-[1029px]:max-w-[300px]"
-        >
+        <a id="box" href="#_" className="">
           <figure className="shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
             <picture>
-              <source srcSet="/men 3.webp" />
-              <img src="/men 3.webp" alt="" loading="lazy" />
+              <source srcSet="/men 1.webp" />
+              <img src="/men 1.webp" alt="" loading="lazy" />
             </picture>
 
             <figcaption>
-              <p className="h-[44px] mb-3 overflow-hidden text-ellipsis">
+              <p className="h-[44px] my-3 overflow-hidden text-ellipsis text-center">
                 The Children&apos;s Place Girls Basic Super skinny Jean- Blue
               </p>
-              <p>$26.2</p>
+              <p className="text-center pb-3 text-xl font-bold text-[#af4261] dark:text-[#f3ec78]">
+                $26.2
+              </p>
             </figcaption>
           </figure>
         </a>
 
-        <a
-          href="#_"
-          className="flex-[0_1_40%] m-4 max-[340px]:m-3 max-w-[250px] min-[1029px]:max-w-[300px] "
-        >
+        <a id="box" href="#_" className="">
           <figure className="shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
             <picture>
-              <source srcSet="/men 4.webp" />
-              <img src="/men 4.webp" alt="" loading="lazy" />
+              <source srcSet="/men 1.webp" />
+              <img src="/men 1.webp" alt="" loading="lazy" />
             </picture>
 
             <figcaption>
-              <p className="h-[44px] mb-3 overflow-hidden text-ellipsis">
-                The Children&apos;s Place Girls Basic Super skinny Jean- Blue
+              <p className="h-[44px] my-3 overflow-hidden text-ellipsis text-center">
+                The Children&apos;s
               </p>
-              <p>$26.2</p>
+              <p className="text-center pb-3 text-xl font-bold text-[#af4261] dark:text-[#f3ec78]">
+                $26.2
+              </p>
             </figcaption>
           </figure>
         </a>
 
-        <a
-          href="#_"
-          className="flex-[0_1_40%] m-4 max-[340px]:m-3 max-w-[250px] min-[1029px]:max-w-[300px]"
-        >
+        <a id="box" href="#_" className="">
           <figure className="shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
             <picture>
-              <source srcSet="/men 5.webp" />
-              <img src="/men 5.webp" alt="" loading="lazy" />
+              <source srcSet="/men 1.webp" />
+              <img src="/men 1.webp" alt="" loading="lazy" />
             </picture>
 
             <figcaption>
-              <p className="h-[44px] mb-3 overflow-hidden text-ellipsis">
+              <p className="h-[44px] my-3 overflow-hidden text-ellipsis text-center">
                 The Children&apos;s Place Girls Basic Super skinny Jean- Blue
               </p>
-              <p>$26.2</p>
+              <p className="text-center pb-3 text-xl font-bold text-[#af4261] dark:text-[#f3ec78]">
+                $26.2
+              </p>
             </figcaption>
           </figure>
         </a>
 
-        <a
-          href="#_"
-          className="flex-[0_1_40%] m-4 max-[340px]:m-3 max-w-[250px] min-[1029px]:max-w-[300px] "
-        >
+        <a id="box" href="#_" className="">
           <figure className="shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
             <picture>
-              <source srcSet="/men 4.webp" />
-              <img src="/men 4.webp" alt="" loading="lazy" />
+              <source srcSet="/men 1.webp" />
+              <img src="/men 1.webp" alt="" loading="lazy" />
             </picture>
 
             <figcaption>
-              <p className="h-[44px] mb-3 overflow-hidden text-ellipsis">
+              <p className="h-[44px] my-3 overflow-hidden text-ellipsis text-center">
                 The Children&apos;s Place Girls Basic Super skinny Jean- Blue
               </p>
-              <p>$26.2</p>
+              <p className="text-center pb-3 text-xl font-bold text-[#af4261] dark:text-[#f3ec78]">
+                $26.2
+              </p>
             </figcaption>
           </figure>
         </a>
